8f06d98970686fb2b22cb069547c315d4dd96ff0
mrwutong <mrwutong@163.com>
2015-07-02 15:19:23 +0800

    支付宝签名算法移至服务端

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # cordova-qdc-alipay
 支付宝APP支付cordova,ionic插件(Android版，Ios版)
 
+* 2015.07.02 支付宝签名算法移至服务端
 * 2015.07.01 支付宝Android集成，初步完成
 
 # 1. Android客户端安装
@@ -28,7 +29,7 @@
 
 ## 1.3 JS调用说明
 
-* 事先前调用后台支付宝API生成订单数据及签名数据
+* 事先前调用后台服务端API生成订单数据及签名数据
 * 调用plugin的JS方法【alipay.payment】进行支付
 
 ```js
@@ -36,16 +37,9 @@
 	# 参数说明：格式为JSON格式
 	# cb_success:调用成功回调方法
 	# cb_failure:调用失败回调方法
-	{
-	partner: 合作者身份ID
-	private_key: 商户私钥
-	notify_url: notify_url
-	seller_id: 卖家支付宝账号
-	subject: 商品名称
-	body: 商品详情
-	total_fee: 总金额
-	out_trade_no:商户网站唯一订单号
-	}
-	注：订单总金额，只能可为小数，单位为【元】。
+	# json: {
+	    pay_info: 支付信息
+	    sign: 密钥
+	  }
 ```
 
--- src/android/Alipay.java
+++ src/android/Alipay.java
@@ -1,7 +1,6 @@
 package com.qdc.plugins.alipay;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.security.MessageDigest;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -20,239 +19,171 @@ import com.alipay.sdk.app.PayTask;
  * 
  */
 public class Alipay extends CordovaPlugin {
-  /** JS回调接口对象 */
-  public static CallbackContext cbContext = null;
+	/** JS回调接口对象 */
+	public static CallbackContext cbContext = null;
 
-  /** LOG TAG */
-  private static final String LOG_TAG = Alipay.class.getSimpleName();
+	/** LOG TAG */
+	private static final String LOG_TAG = Alipay.class.getSimpleName();
 
-  /**
-   * 插件主入口
-   */
-  @Override
-  public boolean execute(String action, final JSONArray args,
-      CallbackContext callbackContext) throws JSONException {
-    LOG.d(LOG_TAG, "Alipay#execute");
+	/**
+	 * 插件主入口
+	 */
+	@Override
+	public boolean execute(String action, final JSONArray args,
+			CallbackContext callbackContext) throws JSONException {
+		LOG.d(LOG_TAG, "Alipay#execute");
 
-    boolean ret = false;
+		boolean ret = false;
 
-    if ("payment".equalsIgnoreCase(action)) {
-      LOG.d(LOG_TAG, "Alipay#payment.start");
+		if ("payment".equalsIgnoreCase(action)) {
+			LOG.d(LOG_TAG, "Alipay#payment.start");
 
-      cbContext = callbackContext;
+			cbContext = callbackContext;
 
-      PluginResult pluginResult = new PluginResult(
-          PluginResult.Status.NO_RESULT);
-      pluginResult.setKeepCallback(true);
-      callbackContext.sendPluginResult(pluginResult);
+			PluginResult pluginResult = new PluginResult(
+					PluginResult.Status.NO_RESULT);
+			pluginResult.setKeepCallback(true);
+			callbackContext.sendPluginResult(pluginResult);
 
-      // 参数检查
-      if (args.length() != 1) {
-        LOG.e(LOG_TAG, "args is empty", new NullPointerException());
-        ret = false;
-        PluginResult result = new PluginResult(
-            PluginResult.Status.ERROR, "args is empty");
-        result.setKeepCallback(true);
-        cbContext.sendPluginResult(result);
-        return ret;
-      }
+			// 参数检查
+			if (args.length() != 1) {
+				LOG.e(LOG_TAG, "args is empty", new NullPointerException());
+				ret = false;
+				PluginResult result = new PluginResult(
+						PluginResult.Status.ERROR, "args is empty");
+				result.setKeepCallback(true);
+				cbContext.sendPluginResult(result);
+				return ret;
+			}
 
-      JSONObject jsonObj = args.getJSONObject(0);
+			JSONObject jsonObj = args.getJSONObject(0);
 
-      final String partner = jsonObj.getString("partner");
-      if (partner == null || "".equals(partner)) {
-        LOG.e(LOG_TAG, "partner is empty", new NullPointerException());
-        ret = false;
-        PluginResult result = new PluginResult(
-            PluginResult.Status.ERROR, "partner is empty");
-        result.setKeepCallback(true);
-        cbContext.sendPluginResult(result);
-        return ret;
-      }
+			final String payInfo = jsonObj.getString("pay_info");
+			if (payInfo == null || "".equals(payInfo)) {
+				LOG.e(LOG_TAG, "pay_info is empty", new NullPointerException());
+				ret = false;
+				PluginResult result = new PluginResult(
+						PluginResult.Status.ERROR, "pay_info is empty");
+				result.setKeepCallback(true);
+				cbContext.sendPluginResult(result);
+				return ret;
+			}
 
-      final String private_key = jsonObj.getString("private_key");
-      if (private_key == null || "".equals(private_key)) {
-        LOG.e(LOG_TAG, "private_key is empty",
-            new NullPointerException());
-        ret = false;
-        PluginResult result = new PluginResult(
-            PluginResult.Status.ERROR, "private_key is empty");
-        result.setKeepCallback(true);
-        cbContext.sendPluginResult(result);
-        return ret;
-      }
+			final String sign = jsonObj.getString("sign");
+			if (sign == null || "".equals(sign)) {
+				LOG.e(LOG_TAG, "sign is empty", new NullPointerException());
+				ret = false;
+				PluginResult result = new PluginResult(
+						PluginResult.Status.ERROR, "sign is empty");
+				result.setKeepCallback(true);
+				cbContext.sendPluginResult(result);
+				return ret;
+			}
 
-      final String notifyUrl = jsonObj.getString("notify_url");
-      if (notifyUrl == null || "".equals(notifyUrl)) {
-        LOG.e(LOG_TAG, "notify_url is empty",
-            new NullPointerException());
-        ret = false;
-        PluginResult result = new PluginResult(
-            PluginResult.Status.ERROR, "notify_url is empty");
-        result.setKeepCallback(true);
-        cbContext.sendPluginResult(result);
-        return ret;
-      }
+			if (!isSameSignature(payInfo, sign)) {
+				LOG.e(LOG_TAG, "pay_info sign failure.",
+						new IllegalStateException());
+				ret = false;
+				PluginResult result = new PluginResult(
+						PluginResult.Status.ERROR, "pay_info sign failure.");
+				result.setKeepCallback(true);
+				cbContext.sendPluginResult(result);
+				return ret;
+			}
 
-      final String sellerId = jsonObj.getString("seller_id");
-      if (sellerId == null || "".equals(sellerId)) {
-        LOG.e(LOG_TAG, "seller_id is empty", new NullPointerException());
-        ret = false;
-        PluginResult result = new PluginResult(
-            PluginResult.Status.ERROR, "seller_id is empty");
-        result.setKeepCallback(true);
-        cbContext.sendPluginResult(result);
-        return ret;
-      }
+			Runnable payRunnable = new Runnable() {
 
-      final String subject = jsonObj.getString("subject");
-      if (subject == null || "".equals(subject)) {
-        LOG.e(LOG_TAG, "subject is empty", new NullPointerException());
-        ret = false;
-        PluginResult result = new PluginResult(
-            PluginResult.Status.ERROR, "subject is empty");
-        result.setKeepCallback(true);
-        cbContext.sendPluginResult(result);
-        return ret;
-      }
+				@Override
+				public void run() {
+					// 构造PayTask 对象
+					PayTask alipay = new PayTask(cordova.getActivity());
 
-      final String body = jsonObj.getString("body");
-      if (body == null || "".equals(body)) {
-        LOG.e(LOG_TAG, "body is empty", new NullPointerException());
-        ret = false;
-        PluginResult result = new PluginResult(
-            PluginResult.Status.ERROR, "body is empty");
-        result.setKeepCallback(true);
-        cbContext.sendPluginResult(result);
-        return ret;
-      }
+					// 查询终端设备是否存在支付宝认证账户
+					boolean isExist = alipay.checkAccountIfExist();
+					if (!isExist) {
+						LOG.e(LOG_TAG, "alipay account is not exists",
+								new IllegalStateException());
+						PluginResult result = new PluginResult(
+								PluginResult.Status.ERROR,
+								"alipay account is not exists");
+						result.setKeepCallback(true);
+						cbContext.sendPluginResult(result);
+						return;
+					}
 
-      final String totalFee = jsonObj.getString("total_fee");
-      if (totalFee == null || "".equals(totalFee)) {
-        LOG.e(LOG_TAG, "total_fee is empty", new NullPointerException());
-        ret = false;
-        PluginResult result = new PluginResult(
-            PluginResult.Status.ERROR, "total_fee is empty");
-        result.setKeepCallback(true);
-        cbContext.sendPluginResult(result);
-        return ret;
-      }
+					// 调用支付接口
+					String resultMsg = alipay.pay(payInfo);
+					LOG.i(LOG_TAG, ">>>>>>>>>>支付回调通知>>>>>>>>>>>");
+					LOG.i(LOG_TAG, resultMsg);
+					LOG.i(LOG_TAG, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 
-      final String outTradeNo = jsonObj.getString("out_trade_no");
-      if (outTradeNo == null || "".equals(outTradeNo)) {
-        LOG.e(LOG_TAG, "out_trade_no is empty",
-            new NullPointerException());
-        ret = false;
-        PluginResult result = new PluginResult(
-            PluginResult.Status.ERROR, "out_trade_no is empty");
-        result.setKeepCallback(true);
-        cbContext.sendPluginResult(result);
-        return ret;
-      }
+					String[] resArr = resultMsg.split(";");
+					JSONObject resJo = new JSONObject();
+					for (String res : resArr) {
+						String[] ress = res.split("=");
+						String key = ress[0];
+						String value = ress[1].substring(1,
+								ress[1].length() - 1);
+						try {
+							resJo.put(key, value);
+						} catch (JSONException e) {
+							LOG.e(LOG_TAG, e.getMessage(), e);
+						}
+					}
 
-      String orderInfo = getOrderInfo(partner, sellerId, subject, body,
-          totalFee, outTradeNo, notifyUrl);
-      String sign = SignUtils.sign(orderInfo, private_key);
-      try {
-        // 仅需对sign 做URL编码
-        sign = URLEncoder.encode(sign, "UTF-8");
-      } catch (UnsupportedEncodingException e) {
-        LOG.e(LOG_TAG, e.getMessage(), e);
-        ret = false;
-        PluginResult result = new PluginResult(
-            PluginResult.Status.ERROR, "sign failure");
-        result.setKeepCallback(true);
-        cbContext.sendPluginResult(result);
-        return ret;
-      }
-      final String payInfo = orderInfo.concat("&sign=\"").concat(sign)
-          .concat("\"&sign_type=\"RSA\"");
+					PluginResult result = new PluginResult(
+							PluginResult.Status.OK, resJo.toString());
+					result.setKeepCallback(true);
+					cbContext.sendPluginResult(result);
+				}
+			};
 
-      Runnable payRunnable = new Runnable() {
+			// 此处必须通过启动线程调起支付
+			Thread payThread = new Thread(payRunnable);
+			payThread.start();
 
-        @Override
-        public void run() {
-          // 构造PayTask 对象
-          PayTask alipay = new PayTask(cordova.getActivity());
+			LOG.d(LOG_TAG, "Alipay#payment.end");
+			return true;
+		}
 
-          // 查询终端设备是否存在支付宝认证账户
-          boolean isExist = alipay.checkAccountIfExist();
-          if (!isExist) {
-            LOG.e(LOG_TAG, "alipay account is not exists",
-                new IllegalStateException());
-            PluginResult result = new PluginResult(
-                PluginResult.Status.ERROR,
-                "alipay account is not exists");
-            result.setKeepCallback(true);
-            cbContext.sendPluginResult(result);
-            return;
-          }
-          
-          // 调用支付接口
-          String resultMsg = alipay.pay(payInfo);
-          LOG.i(LOG_TAG, ">>>>>>>>>>支付回调通知>>>>>>>>>>>");
-          LOG.i(LOG_TAG, resultMsg);
-          LOG.i(LOG_TAG, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+		return true;
+	}
 
-          String[] resArr = resultMsg.split(";");
-          JSONObject resJo = new JSONObject();
-          for (String res : resArr) {
-            String[] ress = res.split("=");
-            String key = ress[0];
-            String value = ress[1].substring(1, ress[1].length() - 1);
-            try {
-              resJo.put(key, value);
-            } catch (JSONException e) {
-              LOG.e(LOG_TAG, e.getMessage(), e);
-            }
-          }
+	/**
+	 * 判断签名是否一致
+	 * 
+	 * @param orign
+	 *            原始值
+	 * @param sign
+	 *            签名
+	 * @return 一致:true
+	 */
+	private boolean isSameSignature(String orign, String sign) {
+		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+				'A', 'B', 'C', 'D', 'E', 'F' };
+		try {
+			byte[] btInput = orign.getBytes();
+			// 获得MD5摘要算法的 MessageDigest 对象
+			MessageDigest mdInst = MessageDigest.getInstance("MD5");
+			// 使用指定的字节更新摘要
+			mdInst.update(btInput);
+			// 获得密文
+			byte[] md = mdInst.digest();
+			// 把密文转换成十六进制的字符串形式
+			int j = md.length;
+			char str[] = new char[j * 2];
+			int k = 0;
+			for (int i = 0; i < j; i++) {
+				byte byte0 = md[i];
+				str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+				str[k++] = hexDigits[byte0 & 0xf];
+			}
 
-          PluginResult result = new PluginResult(PluginResult.Status.OK,
-              resJo.toString());
-          result.setKeepCallback(true);
-          cbContext.sendPluginResult(result);
-        }
-      };
-
-      // 此处必须通过启动线程调起支付
-      Thread payThread = new Thread(payRunnable);
-      payThread.start();
-
-      LOG.d(LOG_TAG, "Alipay#payment.end");
-      return true;
-    }
-
-    return true;
-  }
-
-  /**
-   * 创建订单信息
-   */
-  private String getOrderInfo(String partner, String sellerId,
-      String subject, String body, String totalFee, String outTradeNo,
-      String notifyUrl) {
-    StringBuilder sb = new StringBuilder();
-    // 合作者身份ID
-    sb.append("partner=\"").append(partner).append("\"");
-    // 卖家支付宝账号
-    sb.append("&seller_id=\"").append(sellerId).append("\"");
-    // 商户网站唯一订单号
-    sb.append("&out_trade_no=\"").append(outTradeNo).append("\"");
-    // 商品名称
-    sb.append("&subject=\"").append(subject).append("\"");
-    // 商品详情
-    sb.append("&body=\"").append(body).append("\"");
-    // 商品金额
-    sb.append("&total_fee=\"").append(totalFee).append("\"");
-    // 服务器异步通知页面路径
-    sb.append("&notify_url=\"").append(notifyUrl).append("\"");
-    // 接口名称， 固定值
-    sb.append("&service=\"mobile.securitypay.pay\"");
-    // 支付类型， 固定值
-    sb.append("&payment_type=\"1\"");
-    // 参数编码， 固定值
-    sb.append("&_input_charset=\"utf-8\"");
-    return sb.toString();
-  }
+			return new String(str).equals(sign);
+		} catch (Exception e) {
+			LOG.e(LOG_TAG, e.getMessage(), e);
+			return false;
+		}
+	}
 
 }
