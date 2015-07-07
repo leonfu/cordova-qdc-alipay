d628e5d86110e2a23d22df296820b11ea38505e0
mrwutong <mrwutong@163.com>
2015-07-07 09:55:48 +0800

    MD5检验加UTF-8

--- src/android/Alipay.java
+++ src/android/Alipay.java
@@ -162,7 +162,7 @@ public class Alipay extends CordovaPlugin {
 		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 				'A', 'B', 'C', 'D', 'E', 'F' };
 		try {
-			byte[] btInput = orign.getBytes();
+			byte[] btInput = orign.getBytes("UTF-8");
 			// 获得MD5摘要算法的 MessageDigest 对象
 			MessageDigest mdInst = MessageDigest.getInstance("MD5");
 			// 使用指定的字节更新摘要
